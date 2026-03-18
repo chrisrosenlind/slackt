413722204660144a4d577810f8bfe8a1ba0d13fd
Christoffer <christoffer.rosenlind@gmail.com>
2026-03-18 17:14:46 +0100

    Render func

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
+import { AppState } from "./types";
+
+const state: AppState = {
+  user: undefined,
+  channel: undefined,
+};
+
 console.log("Running");
---
+++ src/types.ts
@@ -0,0 +1,4 @@
+export type AppState = {
+  user?: string;
+  channel?: string;
+};
