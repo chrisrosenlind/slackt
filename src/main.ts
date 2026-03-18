78db9567b81c8f4579de6a5ed406cf1cfd9dbd36
Christoffer <christoffer.rosenlind@gmail.com>
2026-03-18 18:52:56 +0100

    Client

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { AppState } from "./types";
+import { render } from "./render";
+import env from "dotenv";
+
+env.config();
 
 const state: AppState = {
   user: undefined,
   channel: undefined,
 };
+
+const client = new WebSocket(process.env.URL ?? "");
 
 console.log("Running");
