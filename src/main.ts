98ca92505a42df5f9e44624f365aed97f76ca4db
Christoffer <christoffer.rosenlind@gmail.com>
2026-03-22 09:44:11 +0100

    Naming

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,12 @@ import env from "dotenv";
 
 env.config();
 
-const SLACK_TOKEN = process.env?.SLACK_TOKEN ?? "";
-const API_URL = process.env?.APIURL ?? "";
+const token = process.env?.SLACK_TOKEN ?? "";
+const url = process.env?.APIURL ?? "";
 
 const state: AppState = {
   user: undefined,
   channel: undefined,
 };
 
-const client = new Client({ token: SLACK_TOKEN, url: API_URL });
+const client = new Client({ token, url });
